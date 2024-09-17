f3204236cd042bf7ff352d4d8fb8d8fa6384e859
Kerrigan <xybermind@gmail.com>
2024-09-18 01:11:51 +0300

    begin rework to svelte

---
+++ frontend/src/routes/canvas/canvas.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let canvas
+</script>
+
+<style>
+    #myCanvas {
+      image-rendering: pixelated;
+      border: 1px solid #000000;
+    }
+</style>
+
+<canvas
+bind:this={canvas} 
+id="myCanvas"
+width="1000"
+height="1000"></canvas>
